34b5dc2c534297278ff5fe28cac7a00e512d6aa8
unknown <jorge.firata@tecsup.edu.pe>
2024-09-12 12:39:23 -0500

    Add all project files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Spark.Sql;
+
+namespace SparkHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una sesión de Spark
+            SparkSession spark = SparkSession
+                .Builder()
+                .AppName("Spark Hola Mundo")
+                .Master("spark://192.168.0.13:7077")  // Dirección del maestro
+                .GetOrCreate();
+
+            // Crear un DataFrame de ejemplo
+            DataFrame dataFrame = spark.Sql("SELECT 'Hola, mundo!' AS Saludo");
+
+            // Mostrar el DataFrame
+            dataFrame.Show();
+
+            // Detener la sesión de Spark
+            spark.Stop();
+        }
+    }
+}
